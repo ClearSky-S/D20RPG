e0398a1fdd40e55e458956c65158d303056f6a10
ClearSky <ggogono2@gmail.com>
2023-12-10 16:33:11 +0900

    3 - Data

---
+++ Assets/Scripts/Data/DataSerializer.cs
@@ -0,0 +1,20 @@
+using UnityEngine;
+
+public interface IDataSerializer : IDependency<IDataSerializer>
+{
+    string Serialize(Data data);
+    Data Deserialize(string json);
+}
+
+public class DataSerializer : IDataSerializer
+{
+    public string Serialize(Data data)
+    {
+        return JsonUtility.ToJson(data);
+    }
+
+    public Data Deserialize(string json)
+    {
+        return JsonUtility.FromJson<Data>(json);
+    }
+}
--- Assets/Scripts/DiceRoll/RandomNumberGenerator.cs
+++ Assets/Scripts/DiceRoll/RandomNumberGenerator.cs
@@ -12,17 +12,3 @@ public struct RandomNumberGenerator : IRandomNumberGenerator
         return Random.Range(minInclusive, maxExclusive);
     }
 }
-public class MockFixedRNG : IRandomNumberGenerator
-{
-    public int fakeOutput;
-
-    public MockFixedRNG(int output)
-    {
-        fakeOutput = output;
-    }
-
-    public int Range(int minInclusive, int maxExclusive)
-    {
-        return fakeOutput;
-    }
-}
